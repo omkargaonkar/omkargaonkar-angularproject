c9ae7a9ac4d7d85991a2a5297f936b0708edd42b
Omkar Gaonkar <omkar_gaonkar@persistent.com>
2023-02-06 17:00:42 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FirstcontentComponent } from './components/firstcontent/firstcontent.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FirstcontentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
